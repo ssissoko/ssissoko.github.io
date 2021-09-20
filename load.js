83b5cdb5e882553037ed6a0a78fd4491ff3495c2
sdnmdsd <ssissoko@mymail.mines.edu>
2021-09-20 10:41:38 -0600

    Updated main loading page

---
+++ load.js
@@ -0,0 +1,19 @@
+/*Script modified from https://codepen.io/Jackthomsonn/pen/wModMm*/
+$(function () {
+  var btn = $(".btn");
+  btn.on("click", function () {
+    $(this).addClass("btn__progress");
+    setTimeout(function () {
+      btn.addClass("btn__progress--fill");
+    }, 500);
+    setTimeout(function () {
+      btn.removeClass("btn__progress--fill");
+    }, 4100);
+    setTimeout(function () {
+      btn.addClass("btn__complete");
+	  $(".container").hide();
+	  $(".hi").hide();
+    $(".wave").hide();
+    }, 4400);
+  });
+});
